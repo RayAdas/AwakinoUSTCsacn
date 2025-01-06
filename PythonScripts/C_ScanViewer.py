38b7754ee9d7d7a6856da85a82e579f212e22e60
WindyAwaki <az1234567890123456@163.com>
2025-01-06 13:51:52 +0800

    将waveViewer的功能合并至C_ScanViewer中

--- PythonScripts/C_ScanViewer.py
+++ PythonScripts/C_ScanViewer.py
@@ -29,7 +29,6 @@ grid_info = {
     'numY': int(config_metadata['Grid']['numY']),
 }
 
-
 # 创建主窗口
 class WaveformViewer:
     def __init__(self, root, data):
@@ -45,25 +44,52 @@ class WaveformViewer:
         self.global_max = np.max(data)
 
         # 创建Matplotlib图像
-        self.fig, self.ax = plt.subplots()
+        self.fig, (self.ax, self.wave_ax) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1, 1]}, figsize=(10, 5))
         self.image = self.ax.imshow(self.data[:, :, self.current_index], cmap='RdBu', vmin=self.global_min, vmax=self.global_max)
         self.ax.set_title(f"Slice: {self.current_index}")
         self.fig.colorbar(self.image, ax=self.ax)
 
-        # 将Matplotlib嵌入到Tkinter窗口
+        # 创建Matplotlib波形
+        self.wave_ax.set_title("Waveform")
+        self.wave_ax.set_xlabel("Index")
+        self.wave_ax.set_ylabel("Value")
+        self.waveform_line, = self.wave_ax.plot([], [], lw=2)
+        self.vertical_line = self.wave_ax.axvline(x=0, color='red', linestyle='--')  # 初始化红色竖线
+
+        # 将Matplot C扫图片嵌入到Tkinter窗口
         self.canvas = FigureCanvasTkAgg(self.fig, master=root)
         self.canvas_widget = self.canvas.get_tk_widget()
         self.canvas_widget.pack(fill=tk.BOTH, expand=True)
+        # 连接鼠标事件
+        self.fig.canvas.mpl_connect("motion_notify_event", self.on_click)
 
         # 创建滑动条
-        self.slider = ttk.Scale(root, from_=0, to=self.depth - 1, orient=tk.HORIZONTAL, command=self.update_plot)
+        self.slider = ttk.Scale(root, from_=0, to=self.depth - 1, orient=tk.HORIZONTAL, command=self.update_img)
         self.slider.pack(fill=tk.X, padx=10, pady=10)
 
-    def update_plot(self, value):
-        self.current_index = int(float(value))  # 确保索引是整数
+    def update_wave(self, idx):
+        # 更新右侧波形图
+        waveform = self.data[idx[1], idx[0], :]
+        self.waveform_line.set_data(range(waveform.shape[0]), waveform)
+        self.wave_ax.relim()
+        self.wave_ax.autoscale_view()
+        self.canvas.draw()
+
+    def update_img(self, value):
+        self.current_index = int(float(value))# 传入的value是个字符串
         self.image.set_data(self.data[:, :, self.current_index])
         self.ax.set_title(f"Slice: {self.current_index}")
+        self.vertical_line.set_xdata([self.current_index, self.current_index])  # 示例红色竖线位置
         self.canvas.draw()
+    
+    def on_click(self, event):
+    # 确保点击发生在当前的 Axes 中
+        if event.inaxes == self.ax:
+            x, y = int(event.xdata), int(event.ydata)  # 获取点击位置的索引
+            print(f"Clicked at: x={x}, y={y}")
+            self.update_wave(idx=[x,y])
+            # value = self.data[y, x, self.current_index]  # 获取对应数据值
+
 
 # 创建Tkinter主窗口
 root = tk.Tk()
